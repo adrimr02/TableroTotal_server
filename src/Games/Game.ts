a2cfb372515888d9c5cf4dfb625b5d48ad09ef5a
Adrián Martínez <adrianmartinezr2002@gmail.com>
2023-12-11 19:54:29 +0100

    Añadida documentacion a la interfaz Game

--- src/Games/Game.ts
+++ src/Games/Game.ts
@@ -3,9 +3,33 @@ import { TicTacToe } from "./TicTacToeGame"
 import {RockPaperScissors} from "./RPS"
 
 interface Game {
+  /**
+   * Function called to start the game. It should include the main loop of the game
+   */
   startGameLoop(): void
+
+  /**
+   * Function called when a player leaves the game.
+   * 
+   * @param playerId Id of the player that left
+   */
   playerLeave(playerId: string): void
+
+  /**
+   * Function called when a new player joins the game.
+   * 
+   * @param playerInfo Includes all the information of the new player.
+   */
   addPlayer(playerInfo: PlayerInfo): boolean
+
+  /**
+   * Function called when one of the players makes their move. 
+   * This function is independent from the main loop and may be called at any time.
+   * It should only update the game state.
+   * 
+   * @param playerId Id of the player that made the move
+   * @param action Information about the movement made. Each game can have its own
+   */
   move(playerId: string, action: unknown): void
 }
 
